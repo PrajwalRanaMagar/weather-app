c1face4909942feeca046376ddaa7299103cc003
Prajwal Rana Magar <102672479+PrajwalRanaMagar@users.noreply.github.com>
2024-01-02 19:49:19 +0545

    Additional Details Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@
 
 import Navbar from "@/components/Navbar";
 import axios from "axios";
+import { format, parseISO } from "date-fns";
 import Image from "next/image";
 // import { useQuery } from '@tanstack/react-query';
 import { useQuery } from "react-query";
+import Container from "./../components/Container";
+import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
+import WeatherIcon from "@/components/WeatherIcon";
+import { getDayOrNightIcon } from "./../utils/getDayOrNightIcon";
 
 // http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=05016ee12b542724dfab9e2d113a601a
 
@@ -63,24 +68,95 @@ interface WeatherData {
   };
 }
 
- export default function Home() {
-  const { isLoading, error, data } = useQuery<WeatherData>('repoData', async() =>
-  {
-    const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`)
-    return data;
-  }
-)
+export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>(
+    "repoData",
+    async () => {
+      // const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`)
+      const { data } = await axios.get(
+        `http://api.openweathermap.org/data/2.5/forecast?q=kathmandu&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+      );
+      return data;
+    }
+  );
 
-console.log("data", data?.city.name);
+  const firstData = data?.list[0];
 
-if (isLoading) return <div className="flex items-center min-h-screen justify-center">
-  <p className="animate-bounce">Loading...</p>
-   </div>
-  ;
+  console.log("data", data);
+
+  if (isLoading)
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    );
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/* today data */}
+        <section className="space-y-4">
+          <div className="space-y-2">
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")} </p>
+              <p className="text-lg">
+                ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})
+              </p>
+            </h2>
+            <Container className="gap-10 px-6 items-center ">
+              {/* temprature */}
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertKelvinToCelsius(firstData?.main.temp ?? 285.17)}°
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels Like</span>
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.feels_like ?? 0)}°
+                  </span>
+                </p>
+                <p className="text-xs space-x-2">
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}
+                    °↓{" "}
+                  </span>
+                  <span>
+                    {" "}
+                    {convertKelvinToCelsius(firstData?.main.temp_max ?? 0)}
+                    °↑
+                  </span>
+                </p>
+              </div>
+              {/* time and weather icon */}
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                {data?.list.map((d, i) => (
+                  <div
+                    key={i}
+                    className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"
+                  >
+                    <p className="whitespace-nowrap">
+                      {format(parseISO(d.dt_txt), "h:mm a")}
+                    </p>
+
+                    {/* <WeatherIcon iconName={d.weather[0].icon}/> */}
+                    <WeatherIcon
+                      iconName={getDayOrNightIcon(d.weather[0].icon, d.dt_txt)}
+                    />
+
+                    <p>{convertKelvinToCelsius(d?.main.temp ?? 0)}°</p>
+                  </div>
+                ))}
+              </div>
+            </Container>
+          </div>
+        </section>
+
+        {/* 7 day forcast data */}
+        <section className="flex w-full flex-col gap-4">
+          <p className="text-2xl">Forcast(7 days)</p>
+        </section>
+      </main>
     </div>
   );
 }
